0f87d5e749bc030cc9019bdf9e73330c1f74982d
Zachary <zpyingling9559@gmail.com>
2020-02-25 04:22:52 -0500

    adding basic information no animations

--- logic.js
+++ logic.js
@@ -1,5 +1,32 @@
 $(document).ready(() => {
-  $("#header-name").hover(() => {
-    $(this).animate(() => {});
-  });
+  if ($(window).width() < 578) {
+    $("#linkedin-footer").removeClass("btn-lg");
+    $("#github-footer").removeClass("btn-lg");
+  }
+
+  if ($(window).width() < 578) {
+    $("#linkedin-footer").addClass("btn-sm");
+    $("#github-footer").addClass("btn-sm");
+  }
+
+  // $(".project-image").hover(
+  //   $(this)
+  //     .stop(true)
+  //     .animate(
+  //       {
+  //         left: "290px"
+  //       },
+  //       "fast"
+  //     ),
+  //   // $(this).fadeIn();
+  //   $(this)
+  //     .stop(true)
+  //     .animate(
+  //       {
+  //         left: "290px"
+  //       },
+  //       "fast"
+  //     )
+  //   // $(this).fadeOut();
+  // );
 });
